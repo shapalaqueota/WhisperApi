a422a6a1402e4ba1616154cbaf1172ad80ec3cc4
Azamat <smagzanoff@gmail.com>
2024-09-20 15:54:58 +0500

    feat: added get method

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import transcriptions
 
 app = FastAPI(
     title="Whisper API",
-    version="1.0.0",
+    version="12.0.0",
+    description="Whisper API HUI",
 )
 
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить запросы с любых источников
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
+# Подключение маршрутов
 app.include_router(transcriptions.router, prefix="/api/v1/transcriptions", tags=["Transcriptions"])
